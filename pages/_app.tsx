16eb809fcbfb280989bf07155c0ce77bc9b4fba8
kimtaegwan <ab154@naver.com>
2022-05-25 22:36:01 +0900

    Edit meta head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,10 @@ import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import "tailwindcss/tailwind.css";
 import "styles/globals.css";
-import preview from "assets/image/preview.png";
-import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <Head>
-        <title>taegwan`s dev blog</title>
-        <meta property="og:title" content="taegwan`s dev blog" />
-        <meta property="og:description" content="개발자 김태관의 기술 블로그" />
-        {/*<meta property="og:image" content={preview} />*/}
-      </Head>
       <Component {...pageProps} />
     </RecoilRoot>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,22 @@ import type { NextPage } from "next";
 import Layout from "layout";
 import MainComponent from "@/components/MainComponent";
 import axios from "axios";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   axios.defaults.withCredentials = true;
 
   return (
     <Layout>
+      <Head>
+        <title>taegwan`s dev blog</title>
+        <meta property="og:title" content="taegwan`s dev blog" />
+        <meta property="og:description" content="개발자 김태관의 기술 블로그" />
+        <meta
+          property="og:image"
+          content="https://taegwan.dev/next/image?url=https%3A%2F%2Felric-blog.s3.ap-northeast-2.amazonaws.com%2FPortfoiloThumbnail%2Fblog.png&w=3840&q=75"
+        />
+      </Head>
       <MainComponent />
     </Layout>
   );

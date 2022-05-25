f24853aaa011ddb54b75c84179d8a3913cdbedce
kimtaegwan <ab154@naver.com>
2022-05-25 22:43:12 +0900

    Edit meta image

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,11 +11,11 @@ const Home: NextPage = () => {
     <Layout>
       <Head>
         <title>taegwan`s dev blog</title>
-        <meta property="og:title" content="taegwan`s dev blog" />
+        <meta property="og:title" content="taegwan's dev blog" />
         <meta property="og:description" content="개발자 김태관의 기술 블로그" />
         <meta
           property="og:image"
-          content="https://taegwan.dev/next/image?url=https%3A%2F%2Felric-blog.s3.ap-northeast-2.amazonaws.com%2FPortfoiloThumbnail%2Fblog.png&w=3840&q=75"
+          content="https://elric-blog.s3.ap-northeast-2.amazonaws.com/PortfoiloThumbnail/blog.png"
         />
       </Head>
       <MainComponent />

b7f375d2506183abd59b15d941cdfd1fb4085183
kimtaegwan <ab154@naver.com>
2022-06-03 22:54:00 +0900

    Edit conflict cord

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,24 +3,19 @@ import axios from "axios";
 import Layout from "layout";
 import MainComponent from "@/components/MainComponent";
 
-import axios from "axios";
 import Head from "next/head";
 
 const Home: NextPage = ({ data }: any) => {
   axios.defaults.withCredentials = true;
   return (
     <Layout>
-
-      <MainComponent datas={data} />
-
       <Head>
         <title>taegwan`s dev blog</title>
         <meta property="og:title" content="taegwan's dev blog" />
         <meta property="og:description" content="개발자 김태관의 기술 블로그" />
         <meta property="og:image" content="https://taegwan.dev/preview.png" />
       </Head>
-      <MainComponent  datas={data} />
-
+      <MainComponent datas={data} />
     </Layout>
   );
 };
